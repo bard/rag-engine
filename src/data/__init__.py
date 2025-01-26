8d2c923aea78b6f4ffd76231b642fadbff2e482d
Massimiliano Mirra <bard@hyperstruct.net>
2025-01-26 22:59:40 +0100

    chore: remove insurance-related code

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,11 +1,9 @@
 from .base import IndexableData
 from .generic_tabular import GenericTabularData
-from .insurance_average_expenditure import InsuranceAverageExpenditureData
 from .textual import TextualData
 
 __all__ = [
     "IndexableData",
     "GenericTabularData",
-    "InsuranceAverageExpenditureData",
     "TextualData",
 ]
